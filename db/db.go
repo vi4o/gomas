84ad79654965a90ef9377b6a3d142ebe4402985c
Veselin Stoyanov <veselin1996@gmail.com>
2017-01-17 17:40:06 +0200

    feature/f2017170101-database-structure - Created basic database project structure. - Planned database structure for constant GameServer data. - Planned database structure for dynamic GameServer data.

---
+++ db/db.go
@@ -0,0 +1,9 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+
+	// _ "github.com/jinzhu/gorm/dialects/mysql"
+	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
